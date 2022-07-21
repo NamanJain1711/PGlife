e33a4874df67056cf501ab126815d1d209fab3a4
NAMAN JAIN <nj44979@gmail.com>
2022-07-21 21:17:11 +0530

    second

---
+++ includes/database_connect.php
@@ -0,0 +1,7 @@
+<?php
+$conn = mysqli_connect("localhost","root","","pglife");
+if(mysqli_connect_error()){
+    echo "Failed to connect";
+    return;
+}
+?>
---
+++ includes/footer.php
@@ -0,0 +1,24 @@
+<div class="footer">
+        <div class="page-container footer-container">
+            <div class="footer-cities">
+                <div class="footer-city">
+                    <a href="property_list.php?city=Delhi">PG in Delhi</a>
+                </div>
+                <div class="footer-city">
+                    <a href="property_list.php?city=Mumbai">PG in Mumbai</a>
+                </div>
+                <div class="footer-city">
+                    <a href="property_list.php?city=Bangalore">PG in Bangalore</a>
+                </div>
+                <div class="footer-city">
+                    <a href="property_list.php?city=Hyderabad">PG in Hyderabad</a>
+                </div>
+            </div>
+            <div class="footer-copyright">© 2020 Copyright PG Life </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    
+    <script type="text/javascript" src="./js/common.js"></script>
+
